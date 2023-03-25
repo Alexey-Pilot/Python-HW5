c95554b8364d8def1dec0c4cdd464353bfe184d2
Alexey <alexragnarek@gmail.com>
2023-03-25 12:58:34 +0500

    Ex28 is complited

---
+++ EX28.py
@@ -0,0 +1,11 @@
+def sum_nums(num1, num2):
+    if num1 == 0 and num2 == 0:
+        return 0
+    elif num1 == 0:
+        return 1 + sum_nums((num1, num2 - 1))
+    elif num2 == 0:
+        return 1 + sum_nums(num1 - 1, num2)
+    return  2 + sum_nums(num1 - 1, num2 - 1)
+
+n, m = 3, 2
+print(sum_nums(n, m))
